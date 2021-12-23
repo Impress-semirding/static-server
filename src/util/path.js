867eda7b3414e7268f3b567701b496374ba2f665
丁薛 <304875853@qq.com>
2021-12-23 17:28:15 +0800

    :art:

---
+++ src/util/path.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const process = require('process');
+
+function getAbsolutePath(p) {
+  const isRoot = path.isAbsolute(p);
+
+  if (isRoot) return p;
+  return path.resolve(process.cwd(), p);
+}
+
+module.exports = {
+  getAbsolutePath
+};
